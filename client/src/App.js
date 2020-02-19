04768fa6b1c3f9341b63a81fb780eb4a4aebbcf3
Marcos Ganine <marcosganine@gmail.com>
2020-02-19 18:04:42 -0300

    Verify api and client connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      health: "Checking"
+    };
+  }
+
+  componentDidMount() {
+    fetch("/health")
+      .then(response => response.json())
+      .then(json => this.setState({ health: json }))
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    const { health } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Hacker News Top Stories</h1>
+        <h2>API Health Status</h2>
+        {JSON.stringify(health)}
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appTitle = getByText(/Hacker News Top Stories/i);
+  expect(appTitle).toBeInTheDocument();
 });

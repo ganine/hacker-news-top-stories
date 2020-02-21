6b67aaa870902c64b21fbb8c9e03922b2f661c2a
Marcos Ganine <marcosganine@gmail.com>
2020-02-21 18:12:32 -0300

    Fetch and render story list

--- client/src/Stories.jsx
+++ client/src/Stories.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import axios from 'axios';
 
 class Stories extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stories: []
+    };
+  }
+
+  fetchData() {
+    axios.get('/api/stories').then(
+      result => {
+        this.setState({
+          stories: result.data
+        });
+      }
+    );
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  renderList() {
+    const { stories } = this.state;
+
+    return (
+      <ol>
+        {stories.map(story => (
+          <li key={story.id}>{story.title}</li>
+        ))}
+      </ol>
+    );
+  }
+
   render() {
     return (
-      <div>
+      <div className="stories">
         <h2>Top 15 Hacker News</h2>
-        <ol className="stories">
-        </ol>
+        {this.renderList()}
       </div>
     );
   }
--- client/src/Stories.spec.js
+++ client/src/Stories.spec.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { wait, render } from '@testing-library/react';
 import Stories from './Stories';
+import axios from 'axios';
+
+jest.mock('axios');
 
 describe('Stories', () => {
-  it('renders a stories list title', () => {
+  it('renders title', () => {
+    axios.get.mockResolvedValue({ data: [] });
+
     const { getByText } = render(<Stories />);
 
     const listTitle = getByText(/Top 15 Hacker News/);
 
     expect(listTitle).toBeInTheDocument();
+  });
+
+  it('renders a stories list', async () => {
+    const story = { id: 1, title: 'Vel et quaerat ut.' };
+    axios.get.mockResolvedValue({ data: [story] });
+
+    const { getByText } = render(<Stories />);
+
+    await wait(() => {
+      expect(getByText(story.title)).toBeInTheDocument();
+    });
   });
 });

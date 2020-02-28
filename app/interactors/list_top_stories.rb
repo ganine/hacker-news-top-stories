1b3d6025227b1e7d23ac438acc3d3c1a12826709
Marcos Ganine <marcosganine@gmail.com>
2020-02-28 17:30:31 -0300

    Update list top stories interactor to use api

--- app/interactors/list_top_stories.rb
+++ app/interactors/list_top_stories.rb
@@ -1,10 +1,33 @@
 class ListTopStories
-  def initialize(amount, stories_repository = StoriesRepository.new)
+  def initialize(
+    amount,
+    hacker_news_api = HackerNewsAPI.new,
+    stories_repository = StoriesRepository.new)
+
     @amount = amount
+    @hacker_news_api = hacker_news_api
     @stories_repository = stories_repository
   end
 
   def execute
-    @stories_repository.most_recent(@amount)
+    hacker_news_stories = @hacker_news_api.top_stories(@amount)
+    stories = build_model_list(hacker_news_stories)
+  end
+
+  private
+
+  def build_model(result)
+    attributes = {
+      item_id: result['id'],
+      title: result['title'],
+      author: result['by'],
+      published_at: Time.at(result['time']),
+      url: result['url']
+    }
+    Story.new(attributes)
+  end
+
+  def build_model_list(results)
+    results.map { |result| build_model(result) }
   end
 end
--- spec/interactors/list_top_stories_spec.rb
+++ spec/interactors/list_top_stories_spec.rb
@@ -1,16 +1,27 @@
 require 'rails_helper'
 
 describe ListTopStories, type: :request do
+  before :all do
+    VCR.insert_cassette 'hacker_news'
+  end
+
+  after :all do
+    VCR.eject_cassette
+  end
+
   describe '#execute' do
+    let(:hacker_news) { HackerNewsAPI.new }
     let(:repository) { double('StoriesRepository') }
     let(:amount) { 15 }
 
-    it 'returns top stories' do
-      interactor = described_class.new(amount, repository)
+    it 'returns top stories from API' do
+      allow(hacker_news).to receive(:top_stories).with(amount).and_call_original
+      interactor = described_class.new(amount, hacker_news, repository)
 
-      expect(repository).to receive(:most_recent).with(amount)
+      stories = interactor.execute
 
-      interactor.execute
+      expect(stories.size).to be_eql(amount)
+      expect(stories.sample).to be_kind_of(Story)
     end
   end
 end

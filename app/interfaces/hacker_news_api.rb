a05c8c8ac571f5888662cc723c4c352722e317a0
Marcos Ganine <marcosganine@gmail.com>
2020-02-29 19:40:00 -0300

    Refactor Hacker News API

--- app/interfaces/hacker_news_api.rb
+++ app/interfaces/hacker_news_api.rb
@@ -1,11 +1,22 @@
 class HackerNewsAPI
-  def top_stories(amount = 15)
-    response = Faraday.get('https://hacker-news.firebaseio.com/v0/topstories.json')
-    stories_ids = JSON.parse(response.body)
+  def initialize
+    @connection = Faraday.new(
+      url: 'https://hacker-news.firebaseio.com/v0/'
+    )
+  end
 
-    stories_ids.take(amount).map do |id|
-      item_response = Faraday.get("https://hacker-news.firebaseio.com/v0/item/#{id}.json")
-      JSON.parse(item_response.body)
-    end
+  def top_stories(amount = 15)
+    top_stories_ids(amount).map { |id| story(id) }
+  end
+
+  def top_stories_ids(amount = 15)
+    response = @connection.get('topstories.json')
+    stories_ids = JSON.parse(response.body)
+    stories_ids.take(amount)
+  end
+
+  def story(item_id)
+    response = @connection.get("item/#{item_id}.json")
+    JSON.parse(response.body)
   end
 end
--- spec/interfaces/hacker_news_api_spec.rb
+++ spec/interfaces/hacker_news_api_spec.rb
@@ -12,13 +12,35 @@ describe HackerNewsAPI do
   let(:hacker_news) { HackerNewsAPI.new }
 
   describe '#top_stories' do
-    it 'fetches top stories from Hacker News API' do
+    it 'fetches top stories' do
       results = hacker_news.top_stories
 
       expect(results).to be_kind_of(Array)
 
-      fields = %w[by descendants id kids score time title type url]
+      fields = %w[by descendants id score time title type url]
       expect(results.sample.keys).to include(*fields)
+    end
+  end
+
+  describe '#top_stories_ids' do
+    it 'fetches top stories ids' do
+      amount = 10
+      results = hacker_news.top_stories_ids(amount)
+
+      expect(results).to be_kind_of(Array)
+      expect(results.sample).to be_kind_of(Integer)
+      expect(results.size).to be_eql(amount)
+    end
+  end
+
+  describe '#story' do
+    it 'fetches an story' do
+      stories_ids = hacker_news.top_stories_ids
+
+      result = hacker_news.story(stories_ids.first)
+
+      fields = %w[by descendants id score time title type url]
+      expect(result.keys).to include(*fields)
     end
   end
 end

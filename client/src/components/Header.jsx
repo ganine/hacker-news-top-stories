806da87a882e717fa3afa4cceb5b90617ae22766
Marcos Ganine <marcosganine@gmail.com>
2020-03-04 13:17:23 -0300

    Add page logo and favicon

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 import './Header.css';
+import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
     <header className="header">
+      <div className="header-logo">
+        <img src={logo} alt="Hacker News logo" />
+      </div>
+
       <h1>Hacker News</h1>
     </header>
   );
